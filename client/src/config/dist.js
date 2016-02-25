c94e9642be66cb38f0b2c7b1b07ce4c44eac4463
Andy Bryant <andybryant@gmail.com>
2016-02-25 19:28:39 +1100

    updated dist ws url

--- client/src/config/dist.js
+++ client/src/config/dist.js
@@ -1,7 +1,8 @@
 
 const config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
-  wsUrl: 'ws://localhost:9000/ws', // 'ws://au2104smjump1.linux.srv.westpac.com.au:9000/ws',
+//  wsUrl: 'ws://localhost:9000/ws',
+  wsUrl: 'ws://au2104smjump1.linux.srv.westpac.com.au:9000/ws',
 };
 
 export default config;
